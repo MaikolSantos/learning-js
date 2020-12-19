960ef0af62b75fb8fb65e36244f55ee770d80f2d
Maikol Lourençon dos Santos <maikol.lourencon@gmail.com>
2020-12-19 12:58:33 -0300

    update

---
+++ javascript-typescript/aula28/index.js
@@ -0,0 +1,18 @@
+// conhcendo o método construtor Date
+
+// const data = new Date(60000); // será adicionado os 60.000 milésimo à data da era INUX, ou seja 1970-01-01 00:00:00 + 60.000 = 1970-01-01 00:01:00
+
+// const data = new Date(); // pega a data atual.
+
+//const data = new Date(1993, 6, 25, 12, 41, 11, 368); // 1993 ano, 6 março, 25 dia, 12 hora, 41 minuto, 11 segundo, 368 milésimos de segundo 
+
+/* const data = new Date('1995-5-27 13:36:54:100');
+console.log('Dia', data.getDate()); 
+console.log('Mês', data.getMonth() + 1); // Mês em js começa no índice 0 (janeiro) até 11(dezembro). +1 para mostrar a número real
+console.log('Ano', data.getFullYear()); 
+console.log('Horas', data.getHours());
+console.log('Minutos', data.getMinutes()); 
+console.log('Segundos', data.getSeconds());
+console.log('Milésimo de Segundos', data.getMilliseconds());
+console.log('Dia da Semana', data.getDay()); // 0 - Domingo, 6 - Sábado
+console.log(data.toString()); // é bom usar toString() para obter a data referente a localidade */
