2de6dc4f6bbb9fecf020a080a9bf737b81ebb6b3
Maikol Lourençon dos Santos <maikol.lourencon@gmail.com>
2020-12-22 16:58:16 -0300

    update

--- javascript-typescript/aula30/assets/js/main.js
+++ javascript-typescript/aula30/assets/js/main.js
@@ -1,18 +1,35 @@
-function main (){
-    const data = new Date();
-    const dia = data.getDay();
-    const num = data.getDate();
-    const mes = data.getMonth() + 1;
-    const ano = data.getFullYear();
-    const horas = data.getHours();
-    const minutos = data.getMinutes();
-    
-    const diasSemana = ['Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira',
-    'Sexta-feira', 'Sábado', 'Domingo'];
+// function main (){
+//     const data = new Date();
+//     const dia = data.getDay();
+//     const numMes = data.getMonth();
 
-    const titulo = document.querySelector('h1');
-    titulo.innerHTML = `${diasSemana[dia]}, ${num} de ${mes} de ${ano} às ${horas}:${minutos}`;
-    
-}
+//     const diaSemana = semana(dia);
 
-main();
+//     const mes = meses(numMes);    
+
+//     const titulo = document.querySelector('.container h1');
+//     titulo.innerHTML = (`${diaSemana}, ${data.getDate()} de ${mes} de ${data.getFullYear()}` +
+//      `às ${zero(data.getHours())}:${zero(data.getMinutes())}`);
+// }
+
+// function semana(num){
+//     const diasSemana = ['Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira',
+//     'Sexta-feira', 'Sábado', 'Domingo'];
+//     return diasSemana[num];
+// }
+
+// function meses(num){
+//     const mes = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 
+//     'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+//     return mes[num];
+// }
+
+// function zero(num){
+//     return num < 10 ? `0${num}` : num;
+// }
+
+// main();
+
+const titulo = document.querySelector('.container h1');
+const data = new Date();
+titulo.innerHTML = data.toLocaleString('pt-BR', { dateStyle: 'full', timeStyle: 'short' });
