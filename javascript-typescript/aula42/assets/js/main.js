037cbac808416526ad9d9ccec70bb6489296c260
Maikol Lourençon dos Santos <maikol.lourencon@gmail.com>
2021-01-08 00:00:22 -0300

    add exercise

---
+++ javascript-typescript/aula42/assets/js/main.js
@@ -0,0 +1,6 @@
+// Escreva uma função chamada ePaisagem que receba 2 argumentos. largura e altura de uma imagem (number).
+// Retorne true se a imagem  estiver no modo paisagem
+
+const ePaisagem = (largura, altura) => largura > altura;
+
+console.log(ePaisagem(5, 10));
