9fd998c856c7b744b9906932dae500f9d2bfbb79
Maikol Lourençon dos Santos <maikol.lourencon@gmail.com>
2021-01-26 16:59:01 -0300

    add exercise timer

---
+++ javascript-typescript/aula47/assets/js/main.js
@@ -0,0 +1,44 @@
+function initialTime(segundos) {
+    let date = new Date(segundos * 1000);
+    return date.toLocaleTimeString('pt-br', {
+        hour12: false,
+        timeZone: 'GMT'
+    });
+}
+const timer = document.querySelector('.timer');
+const start = document.querySelector('.start');
+const pause = document.querySelector('.pause');
+const reset = document.querySelector('.reset');
+let segundos = 0;
+let clock;
+
+function startTimer(){
+    clock = setInterval(function (){
+        segundos++
+        timer.innerHTML = initialTime(segundos);
+    }, 1000)
+}
+
+
+start.addEventListener('click', (event) => {
+    clearInterval(clock);
+    timer.classList.remove('paused')
+    startTimer()
+});
+
+pause.addEventListener('click', (event) => {
+    if (segundos !== 0){
+        timer.classList.add('paused');
+        clearInterval(clock);
+    } else {
+        clearInterval(clock);
+    }
+});
+
+reset.addEventListener('click', (event) => {
+    clearInterval(clock);
+    timer.classList.remove('paused')
+    segundos = 0;
+    timer.innerHTML = `00:00:00`;
+});
+
